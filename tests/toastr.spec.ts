62af21640a74c9cbd63613ae5bfb1a554e752858
Felipanjos <61696148+Felipanjos@users.noreply.github.com>
2024-12-08 08:12:37 -0300

    checkboxes

--- tests/toastr.spec.ts
+++ tests/toastr.spec.ts
@@ -11,5 +11,29 @@ test.describe('Checkboxes', () => {
     test('Click on first checkbox', async ({ page }) => {
         const firstCheckbox = page.getByRole('checkbox', { name: "Hide on click"} );
         await expect(firstCheckbox).toBeChecked();
+
+        // Recommended method of handling checkboxes is setCheck() instead of click() or check()/uncheck()
+        await firstCheckbox.setChecked(false, { force:true });
+        await expect(firstCheckbox).not.toBeChecked();
+
+        await firstCheckbox.setChecked(true, { force:true });
+        await expect(firstCheckbox).toBeChecked();
+
+        const secondCheckbox = page.getByRole('checkbox', { name: "Prevent arising of duplicate toast" });
+        await expect(secondCheckbox).not.toBeChecked();
+
+        await secondCheckbox.setChecked(true, { force:true });
+        await expect(secondCheckbox).toBeChecked();
+
+        // Unselect all cheeckboxes
+        const allCheckBoxes = page.getByRole('checkbox');
+        
+        // all() will create an array from those locators
+        for (let box of await allCheckBoxes.all()){
+            await box.setChecked(true, { force:true });
+            await expect(box).toBeChecked();
+            await box.setChecked(false, { force:true });
+            await expect(box).not.toBeChecked();
+        }
     });
 });
