bdd9a860b0aa59210f7d1bce710a9b82c21e697d
Felipanjos <61696148+Felipanjos@users.noreply.github.com>
2024-12-11 09:31:00 -0300

    dialogs and improvements to original solution

---
+++ tests/dialog.spec.ts
@@ -0,0 +1,24 @@
+import { test, expect } from '@playwright/test'; 
+
+test.beforeEach( async ({ page }) => {
+    await page.goto('http://localhost:4200');
+    await page.getByTitle('Tables & Data').click();
+    await page.getByTitle('Smart Table').click();
+    await expect(page).toHaveURL('http://localhost:4200/pages/tables/smart-table');
+});
+
+test.describe('Dialog', () => {
+    test('something', async ({ page }) => {
+        const targetMail = "mdo@gmail.com";
+        const targetRow = page.getByRole('table').locator('tr', { hasText: targetMail });
+
+        page.once('dialog', async dialog => {
+            expect(dialog.message()).toEqual('Are you sure you want to delete?');
+            await dialog.accept();
+        });
+
+        await targetRow.locator('.nb-trash').click();
+        
+        await expect(targetRow).toHaveCount(0);
+    });
+});
